014e12704840a052ae75868d1ff33c0d23ffd539
Tracy Tucker <tracytuckeremail@gmail.com>
2022-04-10 14:09:02 -0500

    dispatching to the store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import store from "./store";
+
+store.dispatch({
+  type: "ADDBUG",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+store.dispatch({
+  type: "REMOVEBUG",
+  payload: {
+    id: 1,
+  },
+});
+
+// store.dispatch({});
+
+console.log(store.getState());
